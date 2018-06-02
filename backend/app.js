88af69ec49b5a17009d3e258cb345b330a20e9cd
momae93 <31903617+momae93@users.noreply.github.com>
2018-06-02 14:18:06 +0200

    [BACKEND][ROUTES] Added basic routes

--- backend/app.js
+++ backend/app.js
@@ -9,4 +9,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', routes)
 
+app.use(function(req, res, next) {
+  var err = new Error('Votre page est introuvable');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function(err, req, res, next) {
+  res.status(err.status)
+  .json({
+    status: err.status,
+    data: {},
+    message: err.message
+  });
+});
+
 module.exports = app
