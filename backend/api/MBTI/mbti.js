4c56690cb30b3cc91af2c5970c1f55769eafe3f9
momae93 <31903617+momae93@users.noreply.github.com>
2018-06-02 17:16:18 +0200

    [BACKEND][MBTI] Modified some variables to make code more maintainable

--- backend/api/MBTI/mbti.js
+++ backend/api/MBTI/mbti.js
@@ -1,11 +1,13 @@
 const db = require('../index');
+const TABLE_NAME =  't_mbti';
 const MESSAGE_OK = 'OK';
 const MESSAGE_ERROR = 'An error occured';
 const MESSAGE_NOT_FOUND = 'No data has been found';
 
 function getAllMBTI(req, res, next) {
-    db.any(`SELECT * FROM t_mbti`)
+    db.any(`SELECT * FROM ${TABLE_NAME}`)
         .then(function (data) {
+            const status = 200
             res.status(200)
                 .json({
                     status: 200,
@@ -14,33 +16,40 @@ function getAllMBTI(req, res, next) {
                 });
         })
         .catch(function (error) {
-            console.log(MESSAGE_ERROR)
+            const status = 403
+            console.log(error);
+            res.status(status)
+                .json({
+                    status: status,
+                    data: {},
+                    message: error.message
+                });
         });
 }
 
 function getSingleMBTIById(req, res, next) {
     const id = req.params.id;
-    db.one('SELECT * FROM t_mbti WHERE id = $1', [id])
+    db.one(`SELECT * FROM ${TABLE_NAME} WHERE id = ${id}`)
         .then(function (data) {
-            res.status(200)
+            const status = 200
+            res.status(status)
                 .json({
-                    status: 200,
+                    status: status,
                     data: data,
                     message: MESSAGE_OK
                 });
         })
         .catch(function (error) {
+            const status = 404;
             console.log(error);
-            res.status(200)
+            res.status(status)
                 .json({
-                    status: 403,
+                    status: status,
                     data: {},
-                    message: MESSAGE_NOT_FOUND
+                    message: error.message
                 });
         });
 }
-
-
 
 module.exports = {
     getAllMBTI,
