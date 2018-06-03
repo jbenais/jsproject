9681328f80f551e3cf57e8d25097753d107731f2
momae93 <31903617+momae93@users.noreply.github.com>
2018-06-03 19:08:47 +0200

    [BACKEND][USER] Posting a user also posting its address

--- backend/src/api/user/user.js
+++ backend/src/api/user/user.js
@@ -2,6 +2,7 @@ const db = require('../index');
 const TABLE_NAME = 't_user';
 const MESSAGE_OK = 'OK';
 const sqlUser = require('../../sql').users;
+const sqlAddress = require('../../sql').address;
 
 function getAllUser(req, res, next) {
     db.any(sqlUser.getAll)
@@ -30,11 +31,34 @@ function postUser(req, res, next) {
     const user = req.body.user_general;
     const address = req.body.user_address;
     db.one(sqlUser.add, user)
+        .then(user_general => {
+            postAddress(res, user_general, address.latitude, address.longitude)
+        })
+        .catch(error => {
+            res.status(400)
+                .json({
+                    status: 400,
+                    data: { error },
+                    message: error.message
+                });
+        });
+}
+
+function postAddress(res, user_general, latitude, longitude) {
+    const address = {
+        id_user: user_general.id,
+        latitude: latitude,
+        longitude: longitude
+    }
+    db.one(sqlAddress.add, address)
         .then(data => {
             res.status(200)
                 .json({
                     status: 200,
-                    data: data,
+                    data: {
+                        user_general: user_general,
+                        user_address: data
+                    },
                     message: MESSAGE_OK
                 });
         })
@@ -42,7 +66,7 @@ function postUser(req, res, next) {
             res.status(400)
                 .json({
                     status: 400,
-                    data: {error},
+                    data: { error },
                     message: error.message
                 });
         });
