87372e46a07de82733e53d032122f5e010cab0fc
Julia Benais <benais.julia@gmail.com>
2018-06-09 10:51:53 +0200

    [MESSAGES] Added Messages files for the message sub page
    Signed-off-by: Julia Benais <benais.julia@gmail.com>

---
+++ frontend/src/components/Messages/MatchesList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+
+export default class MatchesList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <List>
+                        <ListItem button>
+                            <ListItemText primary="Nom" />
+                        </ListItem>
+                        <ListItem button component="a">
+                            <ListItemText primary="Nom2" />
+                        </ListItem>
+                    </List>
+                </div>
+        )
+    }
+}
