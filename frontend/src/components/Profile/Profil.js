e06c5398ca22611192ef8ecdbec582357294a1a2
Julia Benais <benais.julia@gmail.com>
2018-06-11 20:33:08 +0200

    [PROFIL] Updated profil page
    Signed-off-by: Julia Benais <benais.julia@gmail.com>

--- frontend/src/components/Profile/Profil.js
+++ frontend/src/components/Profile/Profil.js
@@ -5,10 +5,20 @@ import FormLabel from '@material-ui/core/FormLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import Icon from '@material-ui/core/Icon';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Select from '@material-ui/core/Select';
+import Settings from '@material-ui/icons/Settings';
+import Button from '@material-ui/core/Button';
+import Chip from '@material-ui/core/Chip';
 import Map from './Map';
 import Slider, { Range, createSliderWithTooltip } from 'rc-slider';
 import 'rc-slider/assets/index.css';
 const SliderWithTooltip = createSliderWithTooltip(Slider);
+const RangewithTooltip = createSliderWithTooltip(Slider.Range);
 
 const profiles = ['ENFJ', 'ENFP', 'ENTJ', 'ENTP',
     'ESFJ', 'ESFP', 'ESTJ', 'ESTP',
@@ -19,70 +29,86 @@ function valueFormatter(v) {
     return `${v} km`;
 }
 
+function ageFormatter(value) {
+    return `${value}`;
+}
+
 export default class Profil extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             userProfile: 'ENFJ',
-
+            targetProfiles: [],
+            edit: true,
         };
+
         this.handleChange = this.handleChange.bind(this);
+        this.handleChipChange = this.handleChipChange.bind(this);
     }
 
     handleChange(name) {
         return (event => {
-            console.log(event.target.value);
             this.setState({
-                [name]: event.target.value,
+                targetProfiles: event.target.value,
             })
         });
     }
 
+    handleChipChange(event) {
+        this.setState({
+            targetProfiles: event.target.value,
+        })
+    }
+
     render() {
         return (
-            <div style={{ display: 'flex', flexDirection: 'row' }}>
+            <div style={{ display: 'flex', flexDirection: 'row', padding: '20px' }}>
                 <div style={{ display: 'flex', flex: 1, flexDirection: 'column' }}>
                     <div style={{ display: 'flex', justifyContent: 'center', height: '30%' }}>
-                        <img src="https://picsum.photos/200/300" />
+                        <img id="img" src="https://picsum.photos/200/300" />
                     </div>
                     <div style={{ display: 'flex', justifyContent: 'center', fontFamily: 'Roboto', fontWeight: 800, fontSize: '18px', paddingTop: '20px' }}>Julia BENAIS</div>
-                    <div style={{ display: 'flex', justifyContent: 'center', fontFamily: 'Roboto', fontWeight: 200 }}>21 ans</div>
+                    <div style={{ display: 'flex', justifyContent: 'center', fontFamily: 'Roboto', fontWeight: 200, paddingBottom: '20px' }}>21 ans</div>
                     <Map />
                 </div>
 
-
-
-
-                <div style={{ display: 'flex', flex: 3, flexDirection: 'column', backgroundColor: '#F8F8F8' }}>
-                    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
-                        <div style={{ display: 'flex', fontFamily: 'Roboto', fontWeight: '700', fontSize: '18px', padding: '20px' }}>
-                            Information principales
-                        </div>
-                        <div style={{ display: 'flex', fontFamily: 'Roboto', fontWeight: '700', fontSize: '18px', padding: '20px' }}>
-                            Préférence Amoureuses
-                        </div>
-                    </div>
-                    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
-                        <div style={{ display: 'flex', backgroundColor: 'white', flexDirection: 'column', padding: '30px', width: '45%' }}>
+                <div style={{ display: 'flex', flex: 3, flexDirection: 'column', paddingTop: '20px' }}>
+                    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', flexWrap: 'wrap' }}>
+                        <div style={{ display: 'flex', backgroundColor: 'white', flexDirection: 'column', padding: '20px', width: '45%' }}>
+                            <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                                <Settings />
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly' }}>
+                                <div id="hexagon">
+                                    <span>✓</span>
+                                </div>
+                                <div style={{ display: 'flex', fontFamily: 'Roboto', fontWeight: '700', fontSize: '18px', color: '#4C4C4C' }}>
+                                    Information principales
+                                </div>
+                            </div>
                             <TextField
+                                disabled={this.state.edit}
                                 value="Benais"
                                 id="name"
                                 label="Nom"
                                 margin="normal"
                             />
                             <TextField
+                                disabled={this.state.edit}
                                 id="name"
                                 label="Prénom"
                                 value="Julia"
                                 margin="normal"
                             />
                             <TextField
+                                disabled={this.state.edit}
                                 id="name"
                                 label="Adresse mail"
                                 value="benais.julia@gmail.com"
                                 margin="normal"
                             />
                             <TextField
+                                disabled={this.state.edit}
                                 id="name"
                                 label="Date de naissance"
                                 value="04-11-1996"
@@ -91,6 +117,7 @@ export default class Profil extends React.Component {
 
                             />
                             <TextField
+                                disabled={this.state.edit}
                                 id="multiline-flexible"
                                 label="Description"
                                 multiline
@@ -100,7 +127,8 @@ export default class Profil extends React.Component {
                             />
 
                             <TextField
-                                id="select-currency-native"
+                                disabled={this.state.edit}
+                                id="name"
                                 select
                                 label="Profil MBTI"
                                 value={this.state.userProfile}
@@ -113,9 +141,28 @@ export default class Profil extends React.Component {
                                     </option>
                                 ))}
                             </TextField>
-
+                            <Button
+                                variant="contained"
+                                style={{ visibility: (this.state.edit ? 'visible' : 'hidden'), fontFamily: 'Roboto', color: 'white', backgroundColor: '#01D2CB', borderRadius: '20px', fontSize: '18px', letterSpacing: '1px', marginTop: '30px' }}
+                            >VALIDER</Button>
                         </div>
-                        <div style={{ display: 'flex', backgroundColor: 'white', flexDirection: 'column', padding: '30px', width: '45%', }}>
+
+
+
+
+
+                        <div style={{ display: 'flex', backgroundColor: 'white', flexDirection: 'column', padding: '20px', width: '45%', }}>
+                            <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                                <Settings />
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', marginBottom: '30px'}}>
+                                <div id="hexagon">
+                                    <span>✓</span>
+                                </div>
+                                <div style={{ display: 'flex', fontFamily: 'Roboto', fontWeight: '700', fontSize: '18px', color: '#4C4C4C', marginBottom: '20px' }}>
+                                    Préférences Amoureuses
+                            </div>
+                            </div>
                             <FormControl component="fieldset">
                                 <FormLabel component="legend">Attirances sexuelles</FormLabel>
                                 <FormGroup style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
@@ -133,29 +180,59 @@ export default class Profil extends React.Component {
                                             <Checkbox
                                                 onChange={this.handleChange}
                                                 value="Homme"
-                                            />
-                                        }
+                                            />}
                                         label="Homme"
                                     />
                                 </FormGroup>
                             </FormControl>
-                            <TextField
-                                id="select-currency-native"
-                                select
-                                label="Profil MBTI"
-                                value={this.state.profile}
-                                onChange={this.handleChange('profile')}
-                                margin="normal"
-                            >
-                                {profiles.map(option => (
-                                    <option key={option} value={option}>
-                                        {option}
-                                    </option>
-                                ))}
-                            </TextField>
-                            <SliderWithTooltip min={0} max={1000} step={50} tipFormatter={valueFormatter} />
+                            <div style={{ display: 'flex', fontFamily: 'Roboto', color: "#757575", fontWeight: 400, paddingTop: '20px', paddingBottom: '40px' }}>
+                                Maximum Distance
+                            </div>
+                            <SliderWithTooltip style={{marginBottom: '20px'}} min={0} max={1000} step={50} tipProps={{ visible: true }} tipFormatter={valueFormatter} />
+
+
+
+                            <FormControl style={{ display: 'flex', flexWrap: 'wrap', marginBottom: '20px'}}>
+                                <InputLabel htmlFor="select-multiple-chip">Préférences MBTI</InputLabel>
+                                <Select
+                                    multiple
+                                    value={this.state.targetProfiles}
+                                    onChange={this.handleChipChange}
+                                    input={<Input id="select-multiple-chip" />}
+                                    renderValue={selected => (
+                                        <div>
+                                            {selected.map(value => <Chip key={value} label={value} />)}
+                                        </div>
+                                    )}
+                                >
+                                    {profiles.map(name => (
+                                        <MenuItem
+                                            key={name}
+                                            value={name}>
+                                            {name}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                            <div style={{ display: 'flex', fontFamily: 'Roboto', color: "#757575", fontWeight: 400, paddingTop: '20px', paddingBottom: '40px' }}>
+                                Age Range
+                            </div>
+                            <RangewithTooltip
+                                allowCross={false}
+                                count={2} min={18}
+                                max={55} step={1}
+                                defaultValue={[18, 25]}
+                                tipFormatter={ageFormatter}
+                                tipProps={{ visible: true }}
+                                style={{marginBottom: '20px'}}
+                            />
+                            <Button
+                                variant="contained"
+                                style={{ visibility: (this.state.edit ? 'visible' : 'hidden'), fontFamily: 'Roboto', color: 'white', backgroundColor: '#01D2CB', borderRadius: '20px', fontSize: '18px', letterSpacing: '1px', marginTop: '30px' }}
+                            >VALIDER</Button>
                         </div>
                     </div>
+
                 </div>
             </div>
         )
