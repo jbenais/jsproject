c163c653783b920a64de05f7754f93545c957dc9
Julia Benais <benais.julia@gmail.com>
2018-06-11 20:32:47 +0200

    [MAP] Updated map component to have search box
    Signed-off-by: Julia Benais <benais.julia@gmail.com>

--- frontend/src/components/Profile/Map.js
+++ frontend/src/components/Profile/Map.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
-
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, } from "react-google-maps";
+const { SearchBox } = require("react-google-maps/lib/components/places/SearchBox");
 
 export default class Map extends React.Component {
     constructor(props) {
@@ -13,17 +13,18 @@ export default class Map extends React.Component {
     render() {
         const GoogleMapsComponent = withScriptjs(withGoogleMap(props =>
             <GoogleMap
+
                 defaultZoom={8}
                 defaultCenter={{ lat: -34.397, lng: 150.644 }}
             >
             </GoogleMap>
         ));
         return (
-            <div style={{display: 'flex'}}>
+            <div>
              <GoogleMapsComponent
-                                googleMapURL={"https://maps.googleapis.com/maps/api/js?key=AIzaSyDPgGF_QN1xOijSLG6WICVL5kI87YP7Hs0&v=3.exp&libraries=geometry,drawing,places"}
+                            googleMapURL={"https://maps.googleapis.com/maps/api/js?key=AIzaSyDPgGF_QN1xOijSLG6WICVL5kI87YP7Hs0&v=3.exp&libraries=geometry,drawing,places"}
                             loadingElement={<div style={{ height: `100%` }} />}
-                            containerElement={<div style={{ height: `250px` }} />}
+                            containerElement={<div style={{ height: `250px`, width: '90%', margin: '0 auto' }} />}
                             mapElement={<div style={{ height: `100%` }} />}
                         />
             </div>
