fede5c2d5c058ad00f9a4a71dc224225f7b66a36
Mickael AU <mickael.au@gmail.com>
2018-06-14 11:45:58 +0200

    [BACKEND][USER] Modified PUT request for new fields

--- backend/src/sql/users/update.sql
+++ backend/src/sql/users/update.sql
@@ -2,9 +2,12 @@ UPDATE t_user SET
     firstname = ${firstname},
     lastname = ${lastname},
     birthdate = ${birthdate},
-    profession = ${profession},
     is_male = ${is_male},
     description = ${description},
+    profession = ${profession},
+    max_distance = ${max_distance},
+    age_min = ${age_min},
+    age_max = ${age_max},
     id_mbti = ${id_mbti},
     id_orientation = ${id_orientation}
 WHERE id = ${id_user}
