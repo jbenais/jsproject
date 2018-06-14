8a3ced966475351aacf9c48cdaad9cae30f43dd2
Julia Benais <benais.julia@gmail.com>
2018-06-14 12:52:04 +0200

    [MAP] Access of lat and lng for the marker done
    Signed-off-by: Julia Benais <benais.julia@gmail.com>

--- frontend/src/components/Profile/Map.js
+++ frontend/src/components/Profile/Map.js
@@ -7,45 +7,53 @@ export default class Map extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            position: {lat: 0.0, lng: 0.0 }
+            mapCenter: { lat: 48.723627, lng: 21.254199900000003 },
+            markers: {
+                position: {
+                    lat: 48.723627,
+                    lng: 21.254199900000003,
+                },
+            },
         }
+        this.handleMapClick = this.handleMapClick.bind(this);
     }
 
-    getLatLng(markerPosition) {
+
+    handleMapClick(event) {
+        this.setState({
+            markers: {
+                position: {
+                    lat: event.latLng.lat(),
+                    lng: event.latLng.lng()
+                }
+            },
+            mapCenter: event.latLng
+        });
+
     }
+
     render() {
-        const that = this;
-        const GoogleMapsComponent = compose(
-            withStateHandlers(() => ({
-                isMarkerShown: false,
-                markerPosition: null,
-              }), {
-                onMapClick: ({ isMarkerShown }) => (e) => ({
-                    markerPosition: e.latLng,
-                    isMarkerShown:true
-                })
-              }),
-              withScriptjs,
-              withGoogleMap
-          )
-              (props =>
-                  <GoogleMap
-                      defaultZoom={8}
-                      defaultCenter={{ lat: -34.397, lng: 150.644 }}
-                      onClick={props.onMapClick}
-                  >
-                      {props.isMarkerShown && <Marker position={props.markerPosition} />}
-          
-                  </GoogleMap>
-              );
+        const GoogleMapsComponent =  withScriptjs(withGoogleMap(props => (
+            <GoogleMap
+                ref={props.onMapLoad}
+                defaultZoom={13}
+                defaultCenter={props.center}
+                onClick={props.onMapClick}
+            >
+                <Marker {...props.markers} />
+            </GoogleMap>
+        )));
+
         return (
             <div>
              <GoogleMapsComponent
-                            onMapClick={this.getLatLng}
-                            googleMapURL={"https://maps.googleapis.com/maps/api/js?key=AIzaSyDPgGF_QN1xOijSLG6WICVL5kI87YP7Hs0&v=3.exp&libraries=geometry,drawing,places"}
-                            loadingElement={<div style={{ height: `100%` }} />}
-                            containerElement={<div style={{ height: `250px`, width: '90%', margin: '0 auto' }} />}
-                            mapElement={<div style={{ height: `100%` }} />}
+                center={this.state.mapCenter}
+                onMapClick={this.handleMapClick}
+                onMarker={this.getLatLng}
+                googleMapURL={"https://maps.googleapis.com/maps/api/js?key=AIzaSyDPgGF_QN1xOijSLG6WICVL5kI87YP7Hs0&v=3.exp&libraries=geometry,drawing,places"}                        loadingElement={<div style={{ height: `100%` }} />}
+                containerElement={<div style={{ height: `250px`, width: '90%', margin: '0 auto' }} />}
+                markers={this.state.markers}
+                mapElement={<div style={{ height: `100%` }} />}
                         />
             </div>
         )
